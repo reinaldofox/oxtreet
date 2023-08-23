19aef8dea8553f07fa98f3655eadeefdc46c2964
Reinaldo Adriano <reinaldofox@gmail.com>
2023-08-23 15:33:46 -0300

    Design da tela de login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import API from '../../utils/API'
 import Dialog from '../../utils/Dialog'
+import BgLogin from '../../assets/img/colored-woman.svg'
 import './Login.css'
 
 const Login = (props) => {
@@ -23,27 +24,30 @@ const Login = (props) => {
     .catch(error => console.log('error', error))
   }  
 
-  return(
-    <Form id='formLogin' method='post' onSubmit={ev => handleLogin(ev)}>
+  return (
+    <div className="oxtreet-login" style={{ backgroundImage: `url(${BgLogin})` }}>
 
-      <Form.Group className="mb-3" controlId="inputLogin">
-        <Form.Label>Login</Form.Label>
-        <Form.Control type="text" required value={loginData.login}
-          onChange={e =>  setLoginData({ ...loginData, login: e.target.value.trim()})} />         
-          </Form.Group>
+      <Form id='formLogin' method='post' onSubmit={ev => handleLogin(ev)}>
 
-      <Form.Group className="mb-3" controlId="inputPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" required value={loginData.password}
-          onChange={e =>  setLoginData({ ...loginData, password: e.target.value.trim()})} />         
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="inputLogin">
+          <Form.Label>Login</Form.Label>
+          <Form.Control type="text" required value={loginData.login}
+            onChange={e =>  setLoginData({ ...loginData, login: e.target.value.trim()})} />         
+            </Form.Group>
 
-      <Button variant="primary" className="w-100 btn-secondary" type="submit"> Entrar </Button>
-      <Form.Switch label={"Manter conectado"} checked={loginData.manterLogado}
-          onChange={e =>  setLoginData({ ...loginData, manterLogado: !loginData.manterLogado})} /> 
-        {/* onClick={() => setManterConectado(!manterConectado)} />  */}
+        <Form.Group className="mb-3" controlId="inputPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" required value={loginData.password}
+            onChange={e =>  setLoginData({ ...loginData, password: e.target.value.trim()})} />         
+        </Form.Group>
 
-    </Form>  
+        <Button variant="primary" className="w-100 btn-secondary" type="submit"> Entrar </Button>
+        <Form.Switch label={"Manter conectado"} checked={loginData.manterLogado}
+            onChange={e =>  setLoginData({ ...loginData, manterLogado: !loginData.manterLogado})} /> 
+          {/* onClick={() => setManterConectado(!manterConectado)} />  */}
+
+      </Form>  
+    </div>
   )
 }
 
